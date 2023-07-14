9b33917102e9eeec0dfe823f729ab99da551376d
Alexander Pankratov <idle1992@outlook.com>
2023-07-14 00:11:48 -0700

    shadow the previous variable, match arm

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 
 use std::io;
 use rand::Rng;
-
+use std::cmp::Ordering;
+ 
 fn main() {
     println!("guess the number!");
 
@@ -17,7 +18,15 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater =>println!("Too big"),
+        Ordering::Equal => println!("You win!")
+    }
 
 }
 
